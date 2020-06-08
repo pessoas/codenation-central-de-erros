97d005fff49e79a1feb6dca10c4f930297e28954
Renato Pessoa <renatopessoaneto@gmail.com>
2020-06-08 01:35:50 -0300

    Implementação inicial da classe log

---
+++ src/main/java/br/com/codenation/centraldeerros/entity/Log.java
@@ -0,0 +1,53 @@
+package br.com.codenation.centraldeerros.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Log {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    private Long id;
+
+    @Getter
+    @Setter
+    private String level;
+
+    @Getter
+    @Setter
+    private String description;
+
+    @Getter
+    @Setter
+    private String eventLog;
+
+    @Getter
+    @Setter
+    private String origin;
+
+    @Getter
+    @Setter
+    @CreatedDate
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime createdAt;
+
+    @Getter
+    @Setter
+    private Long eventNumber;
+}
--- src/test/java/br/com/codenation/centraldeerros/CentraldeerrosApplicationTests.java
+++ src/test/java/br/com/codenation/centraldeerros/CentraldeerrosApplicationTests.java
@@ -1,8 +1,12 @@
 package br.com.codenation.centraldeerros;
 
+import br.com.codenation.centraldeerros.entity.Log;
 import br.com.codenation.centraldeerros.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+import java.time.Month;
 
 @SpringBootTest
 class CentraldeerrosApplicationTests {
@@ -31,6 +35,44 @@ class CentraldeerrosApplicationTests {
 
 		assert user.getEmail().equals("modificado@gmail.com");
 		assert user.getPassword().equals("456");
+	}
+
+	@Test
+	void logClassCreation(){
+		LocalDateTime date = LocalDateTime.now();
+
+		Log log = new Log(1L, "level", "descricao", "log","origem",date, 1L);
+
+		assert log.getId().equals(1L);
+		assert log.getLevel().equals("level");
+		assert log.getDescription().equals("descricao");
+		assert log.getEventLog().equals("log");
+		assert log.getOrigin().equals("origem");
+		assert log.getEventNumber().equals(1L);
+		assert log.getCreatedAt().equals(date);
+	}
+
+	@Test
+	void logClassModification(){
+		LocalDateTime date = LocalDateTime.now();
+
+		Log log = new Log(1L, "level", "descricao", "log","origem",date, 1L);
+
+		log.setLevel("level2");
+		log.setDescription("descricao2");
+		log.setEventLog("log2");
+		log.setOrigin("origem2");
+		log.setEventNumber(2L);
+		date = LocalDateTime.of(2014, Month.JANUARY, 1, 10, 10, 30);
+		log.setCreatedAt(date);
+
+		assert log.getId().equals(1L);
+		assert log.getLevel().equals("level2");
+		assert log.getDescription().equals("descricao2");
+		assert log.getEventLog().equals("log2");
+		assert log.getOrigin().equals("origem2");
+		assert log.getEventNumber().equals(2L);
+		assert log.getCreatedAt().equals(date);
 	}
 
 }
